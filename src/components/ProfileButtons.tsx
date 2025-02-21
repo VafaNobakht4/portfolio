79a8a46bf9a96107a956dec36e9f001030952f24
Vafa <vafa.n@greenhabitat.at>
2025-02-22 00:11:38 +0330

    feat: add functionality for buttons for cv and social medias

--- src/components/ProfileButtons.tsx
+++ src/components/ProfileButtons.tsx
@@ -1,30 +1,45 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { Download, Github, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 const ProfileButtons = () => {
   return (
     <div className="flex flex-row gap-x-4">
-      <Button
-        className="w-48 h-12 text-lg text-white font-semibold mt-2 bg-black border border-white hover:bg-[#1c1c1c] hover:text-white"
-        variant="outline"
+      <Link href="./VafaNobakht_Resume.pdf" download="Vafa_Nobakht_CV.pdf">
+        <Button
+          className="w-48 h-12 text-lg text-white font-semibold mt-2 bg-black border border-white hover:bg-[#1c1c1c] hover:text-white"
+          variant="outline"
+        >
+          <Download className="mr-2 !h-5 !w-5 font-semibold" /> Download CV
+        </Button>
+      </Link>
+      <Link
+        href="https://github.com/VafaNobakht4/"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <Download className="mr-2 !h-5 !w-5 font-semibold" /> Download CV
-      </Button>
-      <Button
-        variant="ghost"
-        size="icon"
-        className="text-white mt-2.5 ml-2 hover:bg-[#1c1c1c] hover:text-white"
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-white mt-2.5 ml-2 hover:bg-[#1c1c1c] hover:text-white"
+        >
+          <Github className="!h-6 !w-6 !mt-3.5" />
+        </Button>
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/vafa-nobakht/"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <Github className="!h-6 !w-6 !mt-3.5" />
-      </Button>
-      <Button
-        variant="ghost"
-        size="icon"
-        className="text-white mt-3 ml-2 hover:bg-[#1c1c1c] hover:text-white"
-      >
-        <Linkedin className="!h-6 !w-6 !mt-3.5" />
-      </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-white mt-3 ml-2 hover:bg-[#1c1c1c] hover:text-white"
+        >
+          <Linkedin className="!h-6 !w-6 !mt-3.5" />
+        </Button>
+      </Link>
     </div>
   );
 };
