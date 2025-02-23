7d0b7f6c0f8f7a78b043035a2c8db55ae0631acb
Vafa <vafa.n@greenhabitat.at>
2025-02-23 10:04:39 +0330

    fix: remove extra scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-black">
+    <div className="min-h-screen w-full bg-black overflow-hidden">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-32">
         <div className="pt-8 sm:pt-12 lg:pt-16">
           <div className="flex flex-col items-center sm:items-start sm:flex-row gap-6 sm:gap-8 lg:gap-12">
